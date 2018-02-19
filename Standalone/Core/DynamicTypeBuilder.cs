b5efb7a1a13366794af973b9d0aa7c2e28f08e26
Adel Vilkov <vilkov.adel@gmail.com>
2018-02-19 15:01:21 +0300

    WIP DynamicTypeBuilder (missing IgnoreChanges and DoNotSave)

--- Standalone/Core/DynamicTypeBuilder.cs
+++ Standalone/Core/DynamicTypeBuilder.cs
@@ -11,14 +11,69 @@ namespace Standalone.Core
         public static Type CreateFrom(SchemaDefinition schema)
         {
             var assembly = AssemblyBuilder.DefineDynamicAssembly(
-                new AssemblyName($"{new Guid()}"),
+                new AssemblyName($"{Guid.NewGuid()}"),
                 AssemblyBuilderAccess.Run
             );
             var module = assembly.DefineDynamicModule("Default");
-            var type = module.DefineType("DynamicType");
+            var type = module.DefineType("DynamicType", 
+                TypeAttributes.Public |
+                TypeAttributes.Class |
+                TypeAttributes.AutoClass |
+                TypeAttributes.AnsiClass |
+                TypeAttributes.BeforeFieldInit |
+                TypeAttributes.AutoLayout, 
+                null);
+            type.DefineDefaultConstructor(MethodAttributes.Public);
+            // TODO IgnoreChanges and DoNotSave attributes 
+            foreach (var field in schema.Fields)
+                CreateProperty(type, field.Name, PropertyTypeResolver.FromName(field.Type));
 
-            // TODO
-            throw new NotImplementedException();
+            return type.CreateType();
+        }
+
+        private static void CreateProperty(TypeBuilder tb, string propertyName, Type propertyType)
+        {
+            var fieldBuilder = 
+                tb.DefineField("_" + propertyName, propertyType, FieldAttributes.Private);
+
+            var propertyBuilder = 
+                tb.DefineProperty(propertyName, PropertyAttributes.HasDefault, 
+                propertyType, null);
+
+            var getPropMthdBldr = 
+                tb.DefineMethod("get_" + propertyName, 
+                MethodAttributes.Public | 
+                MethodAttributes.SpecialName | 
+                MethodAttributes.HideBySig, 
+                propertyType, Type.EmptyTypes);
+            var getIl = getPropMthdBldr.GetILGenerator();
+
+            getIl.Emit(OpCodes.Ldarg_0);
+            getIl.Emit(OpCodes.Ldfld, fieldBuilder);
+            getIl.Emit(OpCodes.Ret);
+
+            var setPropMthdBldr =
+                tb.DefineMethod("set_" + propertyName,
+                  MethodAttributes.Public |
+                  MethodAttributes.SpecialName |
+                  MethodAttributes.HideBySig,
+                  null, new[] { propertyType });
+
+            var setIl = setPropMthdBldr.GetILGenerator();
+            var modifyProperty = setIl.DefineLabel();
+            var exitSet = setIl.DefineLabel();
+
+            setIl.MarkLabel(modifyProperty);
+            setIl.Emit(OpCodes.Ldarg_0);
+            setIl.Emit(OpCodes.Ldarg_1);
+            setIl.Emit(OpCodes.Stfld, fieldBuilder);
+
+            setIl.Emit(OpCodes.Nop);
+            setIl.MarkLabel(exitSet);
+            setIl.Emit(OpCodes.Ret);
+
+            propertyBuilder.SetGetMethod(getPropMthdBldr);
+            propertyBuilder.SetSetMethod(setPropMthdBldr);
         }
     }
 }
--- Tests/Standalone/Core/DynamicTypeBuilderTest.cs
+++ Tests/Standalone/Core/DynamicTypeBuilderTest.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using Diffstore.Snapshots;
 using Standalone.Core;
 using Xunit;
 
@@ -25,6 +27,31 @@ namespace Tests.Standalone.Core
             Assert.Contains(properties, p =>
                 p.Name == "IntField" &&
                 p.PropertyType == typeof(int));
+        }
+
+        [Fact]
+        public void ShouldAddAttributesIfSpecified()
+        {
+            var schema = new SchemaDefinition()
+                .WithField("SavedField", "string")
+                .WithField("OnlyActualField", "string", ignoreChanges: true)
+                .WithField("IgnoredField", "string", doNotSave: true);
+            
+            var type = DynamicTypeBuilder.CreateFrom(schema);
+            var properties = type.GetProperties();
+
+            Assert.Equal(3, properties.Length);
+            Assert.Contains(properties, p =>
+                p.Name == "SavedField" && 
+                !p.GetCustomAttributes(false).Any());
+            Assert.Contains(properties, p =>
+                p.Name == "OnlyActualField" &&
+                p.GetCustomAttributes(false).Length == 1 &&
+                Attribute.IsDefined(p, typeof(IgnoreChangesAttribute)));
+            Assert.Contains(properties, p =>
+                p.Name == "IgnoredField" &&
+                p.GetCustomAttributes(false).Length == 1 &&
+                Attribute.IsDefined(p, typeof(IgnoreChangesAttribute)));
         }
     }
 }
