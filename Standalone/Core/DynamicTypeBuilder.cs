eec2c62f40e62452269117ceeb6901653e440973
Adel Vilkov <vilkov.adel@gmail.com>
2018-03-09 13:21:03 +0300

    WIP New codegeneration (using CSharpCompilation)

--- Standalone/Core/DynamicTypeBuilder.cs
+++ Standalone/Core/DynamicTypeBuilder.cs
@@ -1,241 +1,86 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.Loader;
+using System.Text;
 using System.Threading;
+using Diffstore;
 using Diffstore.Entities;
 using Diffstore.Snapshots;
 using FluentIL;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace Standalone.Core
 {
     public static class DynamicTypeBuilder
     {
-        private struct PropertyEmitInfo
-        {
-            public MethodBuilder Getter;
-            public MethodBuilder Setter;
-            public FieldBuilder BackingField;
-        }
-
         public static Type CreateFrom(SchemaDefinition schema)
         {
-            var assembly = AssemblyBuilder.DefineDynamicAssembly(
-                new AssemblyName($"{Guid.NewGuid()}"),
-                AssemblyBuilderAccess.Run
+            var references = CollectReferences(
+                typeof(object), typeof(Attribute), typeof(IDiffstore<,>)
             );
-            var module = assembly.DefineDynamicModule("Default");
-            var type = module.DefineType("DynamicType",
-                TypeAttributes.Public |
-                TypeAttributes.Class |
-                TypeAttributes.AutoClass |
-                TypeAttributes.AnsiClass |
-                TypeAttributes.BeforeFieldInit |
-                TypeAttributes.AutoLayout,
-                null);
-            type.DefineDefaultConstructor(MethodAttributes.Public);
-            var properties = schema.Fields.Select(field =>
-                CreateProperty(type,
-                    field.Name,
-                    TypeResolver.FromName(field.Type),
-                    field.IgnoreChanges,
-                    field.DoNotPersist)
-                    );
+            var compilation = CSharpCompilation.Create($"{Guid.NewGuid()}")
+                .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+                .AddReferences(references)
+                .AddSyntaxTrees(GenerateCodeFor(schema));
 
-            CreateEquals(type, properties);
-            CreateGetHashCode(type, properties);
-            return type.CreateType();
-        }
-
-        private static PropertyEmitInfo CreateProperty(
-            TypeBuilder tb, string propertyName,
-            Type propertyType, bool ignoreChanges = false, bool doNotPersist = false)
-        {
-            var fieldBuilder =
-                tb.DefineField("_" + propertyName, propertyType, FieldAttributes.Private);
-
-            var propertyBuilder =
-                tb.DefineProperty(propertyName, PropertyAttributes.HasDefault,
-                propertyType, null);
-
-            if (ignoreChanges) propertyBuilder.AddAttribute<IgnoreChangesAttribute>();
-            if (doNotPersist) propertyBuilder.AddAttribute<DoNotPersistAttribute>();
-
-            var getPropMthdBldr =
-                tb.DefineMethod("get_" + propertyName,
-                MethodAttributes.Public |
-                MethodAttributes.SpecialName |
-                MethodAttributes.HideBySig,
-                propertyType, Type.EmptyTypes);
-            var getIl = getPropMthdBldr.GetILGenerator();
-
-            getIl.Emit(OpCodes.Ldarg_0);
-            getIl.Emit(OpCodes.Ldfld, fieldBuilder);
-            getIl.Emit(OpCodes.Ret);
-
-            var setPropMthdBldr =
-                tb.DefineMethod("set_" + propertyName,
-                  MethodAttributes.Public |
-                  MethodAttributes.SpecialName |
-                  MethodAttributes.HideBySig,
-                  null, new[] { propertyType });
-
-            var setIl = setPropMthdBldr.GetILGenerator();
-            var modifyProperty = setIl.DefineLabel();
-            var exitSet = setIl.DefineLabel();
-
-            setIl.MarkLabel(modifyProperty);
-            setIl.Emit(OpCodes.Ldarg_0);
-            setIl.Emit(OpCodes.Ldarg_1);
-            setIl.Emit(OpCodes.Stfld, fieldBuilder);
-
-            setIl.Emit(OpCodes.Nop);
-            setIl.MarkLabel(exitSet);
-            setIl.Emit(OpCodes.Ret);
-
-            propertyBuilder.SetGetMethod(getPropMthdBldr);
-            propertyBuilder.SetSetMethod(setPropMthdBldr);
-            return new PropertyEmitInfo()
+            using (var ms = new MemoryStream())
             {
-                Getter = getPropMthdBldr,
-                Setter = setPropMthdBldr,
-                BackingField = fieldBuilder
-            };
-        }
-
-        private static void AddAttribute<T>(this PropertyBuilder builder)
-        {
-            var type = typeof(T);
-            var ctorInfo = type.GetConstructor(new Type[] { });
-            var attr = new CustomAttributeBuilder(ctorInfo, new object[] { });
-            builder.SetCustomAttribute(attr);
-        }
-
-        private static void CreateEquals(TypeBuilder type,
-            IEnumerable<PropertyEmitInfo> properties)
-        {
-            var getMethods = properties
-                .Select(m => m.Getter)
-                .ToList();
-
-            // private bool Equals(DynamicType other)
-            var equalsImpl = type.DefineMethod("Equals",
-                MethodAttributes.Private |
-                MethodAttributes.HideBySig,
-                CallingConventions.HasThis,
-                typeof(bool),
-                Type.EmptyTypes);
-            equalsImpl.SetParameters(equalsImpl.DeclaringType);
-
-            var impl = equalsImpl.GetILGenerator();
-            getMethods.ForEach(m =>
-            {
-                impl.Emit(OpCodes.Ldarg_0); // this
-                impl.EmitCall(OpCodes.Call, m, Type.EmptyTypes);
-                impl.Emit(OpCodes.Ldarg_1); // DynamicType other
-                impl.EmitCall(OpCodes.Call, m, Type.EmptyTypes);
-                if (m.ReturnType.IsPrimitive)
-                    impl.Emit(OpCodes.Ceq);
-                else
+                var result = compilation.Emit(ms);
+                if (!result.Success)
                 {
-                    var equals = typeof(Object).GetMethod("Equals", new[] {
-                        typeof(Object), typeof(Object)
-                    });
-                    impl.EmitCall(OpCodes.Call, equals, Type.EmptyTypes);
+                    throw new NotSupportedException("Could not compile schema class: " +
+                        result.Diagnostics.Aggregate("", (cur, next) =>
+                            cur += " " + next.GetMessage()
+                        ));
                 }
-            });
-
-            for (int i = 0; i < getMethods.Count - 1; i++)
-                impl.Emit(OpCodes.And);
-
-            impl.Emit(OpCodes.Ret);
-
-            // public override bool Equals(object other)
-            var equalsMethod = type.DefineMethod("Equals",
-                MethodAttributes.Public |
-                MethodAttributes.Virtual |
-                MethodAttributes.HideBySig,
-                CallingConventions.HasThis,
-                typeof(bool),
-                new[] { typeof(object) });
-
-            var equalsIl = equalsMethod.GetILGenerator();
-            equalsIl.Emit(OpCodes.Ldarg_0); // this
-            equalsIl.Emit(OpCodes.Ldarg_1); // object other
-            equalsIl.Emit(OpCodes.Isinst, equalsMethod.DeclaringType);
-            // return Equals(other as DynamicType)
-            equalsIl.EmitCall(OpCodes.Call, equalsImpl, Type.EmptyTypes);
-            equalsIl.Emit(OpCodes.Ret);
+                ms.Seek(0, SeekOrigin.Begin);
+                var loadContext = AssemblyLoadContext.Default;
+                var assembly = loadContext.LoadFromStream(ms);
+                return assembly.GetType("EntityType");
+            }
         }
 
-        private static void CreateGetHashCode(TypeBuilder type,
-            IEnumerable<PropertyEmitInfo> properties)
+        private static SyntaxTree GenerateCodeFor(SchemaDefinition schema)
         {
-            var fields = properties
-                .Select(p => p.BackingField)
+            var ignoreChangesAttr = $"{typeof(IgnoreChangesAttribute)}";
+            var doNotPersistAttr = $"{typeof(DoNotPersistAttribute)}";
+            var code = new StringBuilder();
+            code.AppendLine("public class EntityType {");
+            foreach (var field in schema.Fields)
+            {
+                if (field.IgnoreChanges) code.AppendLine($"[{ignoreChangesAttr}]");
+                if (field.DoNotPersist) code.AppendLine($"[{doNotPersistAttr}]");
+                code.AppendLine($"public {field.Type} {field.Name} " + " { get; set; }");
+            }
+            code.AppendLine("}");
+            return CSharpSyntaxTree.ParseText($"{code}");
+        }
+
+        private static List<MetadataReference> CollectReferences(params Type[] types)
+        {
+            var assemblies = new HashSet<Assembly>();
+            types.ToList()
+                .ForEach(type => Collect(type.Assembly));
+
+            return assemblies
+                .Select(assembly => MetadataReference.CreateFromFile(assembly.Location))
+                .Cast<MetadataReference>()
                 .ToList();
 
-            var refHashHelper = CreateRefHashHelper(type);
-
-            // public override int GetHashCode() 
-            var getHashCodeImpl = type.DefineMethod("GetHashCode",
-                MethodAttributes.Public |
-                MethodAttributes.Virtual |
-                MethodAttributes.HideBySig,
-                CallingConventions.HasThis,
-                typeof(int),
-                Type.EmptyTypes);
-
-            var il = getHashCodeImpl.GetILGenerator();
-            il.DeclareLocal(typeof(long));
-            il.Emit(OpCodes.Ldc_I8, 0L);
-            il.Emit(OpCodes.Stloc_0);
-
-            fields.ForEach(f =>
+            void Collect(Assembly assembly)
             {
-                // hash = (hash * 397) ^ value
-                il.Emit(OpCodes.Ldarg_0);
-                il.Emit(OpCodes.Ldfld, f);
-                if (!f.FieldType.IsPrimitive)
-                    il.EmitCall(OpCodes.Call, refHashHelper, Type.EmptyTypes);
-                il.Emit(OpCodes.Conv_I8); // expand to int64
-                il.Emit(OpCodes.Ldloc_0);
-                il.Emit(OpCodes.Ldc_I8, 397L);
-                il.Emit(OpCodes.Mul);
-                il.Emit(OpCodes.Xor);
-                il.Emit(OpCodes.Stloc_0);
-            });
-            il.Emit(OpCodes.Ldloc_0);
-            il.Emit(OpCodes.Conv_I4); // cast back to int32
-            il.Emit(OpCodes.Ret);
-        }
+                if (!assemblies.Add(assembly))
+                    return;
 
-        private static MethodBuilder CreateRefHashHelper(TypeBuilder type)
-        {
-            // private int RefHash(object obj)
-            var refHashHelper = type.DefineMethod("RefHash",
-                MethodAttributes.Private |
-                MethodAttributes.Static |
-                MethodAttributes.HideBySig,
-                CallingConventions.Standard,
-                typeof(int),
-                new[] { typeof(object) });
-
-            var il = refHashHelper.GetILGenerator();
-            var notNullLabel = il.DefineLabel();
-
-            il.Emit(OpCodes.Ldarg_0);
-            il.Emit(OpCodes.Brtrue_S, notNullLabel);
-            il.Emit(OpCodes.Ldc_I4_0);
-            il.Emit(OpCodes.Ret);
-            il.MarkLabel(notNullLabel);
-            il.Emit(OpCodes.Ldarg_0);
-            il.EmitCall(OpCodes.Callvirt, 
-                typeof(object).GetMethod("GetHashCode"),
-                Type.EmptyTypes);
-            il.Emit(OpCodes.Ret);
-            return refHashHelper;
+                assembly.GetReferencedAssemblies()
+                    .ToList()
+                    .ForEach(name => assemblies.Add(Assembly.Load(name)));
+            }
         }
     }
 }
